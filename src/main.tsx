a4d43c5bbd6ecb56d0fecbc9b58fcf39fd1a7247
anna <ana.pego@estudante.ifms.edu.br>
2025-01-22 09:04:21 -0400

    Atualização 22/01

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,33 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import CadastroProduto from './componentes/cadastroproduto/CadastroProduto.tsx';
+import AlterarProduto from './componentes/alterarproduto/AlterarProduto.tsx';
+import Header from './componentes/header/Header.tsx';
+
+//Crie um comentário explicando o que faz o código abaixo
+//Cria uma instância do BrowserRouter e passa um array de objetos com as rotas da aplicação
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <><Header/><App /></>,
+  },
+  {
+    path: "/cadastro-produto",
+    element: <><Header/><CadastroProduto/></>,
+  },
+  {
+    path: "/alterar-produto/:id",
+    element: <><Header/><AlterarProduto/></>,
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
